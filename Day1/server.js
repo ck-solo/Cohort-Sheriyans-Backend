72649aaff7e03e15a0479b3ea0b1e315dd738940
Lucy <solouser019@gmail.com>
2025-07-05 16:21:02 +0530

    completed the first class and things we learn creating the server and diff btw npm modules

--- Day1/server.js
+++ Day1/server.js
@@ -1,3 +1,16 @@
-const catMe = require('cat-me')
+// const catMe = require('cat-me')
 
-console.log(catMe())
+// console.log(catMe())
+
+
+// Creating server
+
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+    res.end("Hello world from the server")
+})
+
+server.listen(3000,()=>{
+    console.log("Server is running on port 3000")
+})
