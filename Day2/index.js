24a3621502100b41ceda6fb2c0d653b621b6caed
Lucy <solouser019@gmail.com>
2025-07-19 18:27:06 +0000

    create server and send req till now

---
+++ Day2/index.js
@@ -0,0 +1,9 @@
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+    res.end("Hello world") // response send krte hai
+}) //server create ho jata hai 
+
+server.listen(3000,()=>{
+    console.log("Server is running on port 3000")
+})
