b39d0a34ab262bc51ac5f482fa8ceedcef6f8e4c
Lucy <solouser019@gmail.com>
2025-07-27 22:55:20 +0530

    an overview of get post delete patch

---
+++ Day4/server.js
@@ -0,0 +1,61 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+let notes = []
+
+app.get('/',(req,res)=>{
+    res.send("Hello, Cohort!")
+})
+
+app.post('/notes',(req,res)=>{
+    console.log(req.body)
+    notes.push(req.body)
+
+    res.json(
+        {
+            message:"notes created successfully", 
+        }
+    )
+})
+
+
+/* Delete /notes/:index  */
+app.delete('/notes/:index',(req,res)=>{
+    const index = req.params.index
+    delete notes[index]
+
+    res.json({
+        message:"Notes deleted successfully"
+    })
+})
+
+
+/* Patch /notes/:index => {title}  */
+
+app.patch("/notes/:index",(req,res)=>{
+    const index = req.params.index
+    const {title} = req.body
+
+    if (!notes[index]) {
+        return res.status(404).json({ message: "Note not found" });
+    }
+    notes[index].title = title
+
+    res.json({
+        message:"Notes updated successfully"
+    })
+})
+
+
+
+
+app.get('/notes',(req,res)=>{
+    res.json(notes)
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running of port 3000")
+})
