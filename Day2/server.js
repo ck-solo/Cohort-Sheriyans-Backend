71d3f280daf1e9db94d1bc6a34ace3f0e94f0fcf
Lucy <solouser019@gmail.com>
2025-07-27 18:01:42 +0530

    Know about server and Know about the work of postman

--- Day2/server.js
+++ Day2/server.js
@@ -3,12 +3,16 @@ const express = require('express')
 // express() // server created
 const app = express()
 
-app.get('/about',(req,res)=>{
-    res.send("Welcome to the About Page");
+app.get("/",(req,res)=>{
+    res.send("Welcome to the website")
 })
 
 app.get('/home',(req,res)=>{
     res.send("Welcome to the Home Page");
+})
+
+app.get('/about',(req,res)=>{
+    res.send("Welcome to the About Page");
 })
 app.listen(3000,()=>{
     console.log("Server is running on port 3000.")
---
+++ Day3/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to the Website .")
+})
+
+app.get('/home',(req,res)=>{
+    res.send("Welcome to the Homme page.")
+})
+
+app.get('/about',(req,res)=>{
+    res.send("Welcome to the About page.")
+})
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000")
+})
