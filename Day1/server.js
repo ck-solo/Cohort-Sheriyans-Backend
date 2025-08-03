a8a5060273994bfc1b005a5030fc191aece57d23
Lucy <solouser019@gmail.com>
2025-08-03 23:21:46 +0530

    restart from day 1

--- Day1/server.js
+++ Day1/server.js
@@ -3,5 +3,5 @@ const http = require('http');
 const server = http.createServer() // server create ho gaya hai
 
 server.listen(3000,()=>{
-    console.log("Server is running on port 3000 ")
+    console.log("Server is running on port 3000 successfully ")
 })
