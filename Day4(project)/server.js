94d8f43ff0112900a6293793c1b2da05215bf78a
Lucy <solouser019@gmail.com>
2025-08-07 16:20:58 +0530

    Delete and update write the whole again and done until get put and delete

---
+++ Day4(project)/server.js
@@ -0,0 +1,48 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+
+let notes = []
+
+app.get('/',(req,res)=>{
+    res.send()
+
+})
+
+app.get('/notes',(req,res)=>{
+    res.json(notes)
+
+})
+
+/* POST /notes  => {title , content} */
+
+app.post('/notes',(req,res)=>{
+    console.log(req.body)
+    notes.push(req.body)
+
+    res.json({
+        message:"notes created successfully"
+    })
+})
+
+
+/*DELETE /notes/:index  */
+
+app.delete('/notes/:index', (req, res)=>{
+    const index = req.params.index;
+    delete notes[index];
+    res.json({
+        message:"notes deleted successfully",
+    });
+
+});
+
+/*PATCH /notes/:index */
+
+
+
+app.listen('3000',()=>{
+    console.log("Server is running of port 3000")
+})
