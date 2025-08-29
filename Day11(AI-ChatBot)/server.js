f2c9114b33af7802aee91632cfedf09d1a18d568
Lucy <solouser019@gmail.com>
2025-08-29 20:16:13 +0530

    fixing the error and create the  chatHistory function

--- Day11(AI-ChatBot)/server.js
+++ Day11(AI-ChatBot)/server.js
@@ -4,9 +4,12 @@ const { createServer } = require("http");
 const { Server } = require("socket.io");
 const httpServer = createServer(app);
 const generateRespone = require("./src/services/ai.services");
+const { userInfo } = require("os");
 const io = new Server(httpServer, {
   /* options */
 });
+
+const chatHistory = [];
 
 io.on("connection", (socket) => {
   console.log("A user connected");
@@ -14,10 +17,20 @@ io.on("connection", (socket) => {
     console.log("A user disconnected");
   });
 
+  
   socket.on('ai-message',async (data)=>{
-    console.log("Message from client", data.prompt)
-    const response = await generateRespone(data.prompt);
-    console.log("Ai-response", response)
+      console.log("Message from client", data.prompt)
+      
+      chatHistory.push({
+        role:"user",
+        parts:[{text:data}]
+      })
+      const response = await generateRespone(chatHistory);
+      
+      chatHistory.push({
+        role:"model",
+        parts:[{text:response}]
+      })
     socket.emit("ai-message-response", { response: response })
   })
 });
--- Day11(AI-ChatBot)/src/services/ai.services.js
+++ Day11(AI-ChatBot)/src/services/ai.services.js
@@ -2,10 +2,10 @@ const { GoogleGenAI } = require("@google/genai");
 
 const ai = new GoogleGenAI({});
 
-async function generateRespone(prompt) {
+async function generateRespone(chatHistory) {
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
-    contents: prompt,
+    contents: chatHistory,
   });
    return response.text;
 }
