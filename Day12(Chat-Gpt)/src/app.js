d7f13a2ba5b355fd8180c2ceacae2abcc9fec380
Lucy <solouser019@gmail.com>
2025-09-03 22:52:01 +0530

    add middleware and rewrite code for better understanding
    - Rewrote project code from scratch as practice
    - Added middleware for improved request handling

--- Day12(Chat-Gpt)/src/app.js
+++ Day12(Chat-Gpt)/src/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const cookieParser = require('cookie-parser')
 const authRouter = require('./routes/app.routes')
+const chatRouter = require('./routes/chat.routes')
 const app = express()
+
+
+
+/*Middleware */
 app.use(express.json())
 app.use(cookieParser())
 
+
+/*Routes */
 app.use('/api/auth/',authRouter)
+app.use('/api/chat/',chatRouter)
 
 module.exports = app;
