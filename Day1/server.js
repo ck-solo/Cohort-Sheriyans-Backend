7885375753647a64d2346a249f7b87399f58e017
Lucy <solouser019@gmail.com>
2025-09-21 21:09:30 +0530

    Revise and Rewrite the code

--- Day1/server.js
+++ Day1/server.js
@@ -1,7 +1,6 @@
 const http = require('http');
 
-const server = http.createServer() // server create ho gaya hai
-
+const server = http.createServer() 
 server.listen(3000,()=>{
     console.log("Server is running on port 3000 successfully ")
 })
